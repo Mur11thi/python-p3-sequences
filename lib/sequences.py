00631f010b19848fdf9eb0756a8c65d2c14ed782
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-05-22 22:02:07 +0300

    submits sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibo_list=[0, 1]
+    if length >1:
+        for _ in range(2,length):
+            fibo_list.append(fibo_list[-1] + fibo_list[-2])
+        print(fibo_list)
+    elif  length <1: print([])    
+    else: print([0])    
+print_fibonacci(9)    
